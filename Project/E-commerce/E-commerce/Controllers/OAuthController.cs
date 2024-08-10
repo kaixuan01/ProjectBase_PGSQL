89ad0229a1f6969a6d293cb46967b26b976f0a62
chong wy <woonyap616@gmail.com>
2024-08-10 16:36:33 +0800

    check in

--- Project/E-commerce/E-commerce/Controllers/OAuthController.cs
+++ Project/E-commerce/E-commerce/Controllers/OAuthController.cs
@@ -1,48 +1,31 @@
 using DAL;
 using DAL.Entity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using E_commerce.Model;
+using Utils.Tools;
 namespace E_commerce.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class OAuthController : ControllerBase
     {
-        public string GenerateJwtToken(string username)
-        {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.UTF8.GetBytes("your_secret_key");
 
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new[]
-                {
-            new Claim(ClaimTypes.Name, username)
-        }),
-                Expires = DateTime.UtcNow.AddHours(1),
-                Issuer = "your_issuer",
-                Audience = "your_audience",
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
-        }
 
         [HttpGet(Name = "GetOAuth")]
         public string Get(User userInfo)
         {
-            if(true)//sucess authorization
-                return GenerateJwtToken(userInfo.UserName);
-            else
+            if(string.IsNullOrEmpty(userInfo.UserName) || string.IsNullOrEmpty(userInfo.Password))
             {
-
+                var _authToken = new AuthToken();
+                if (true)//sucess authorization
+                {
+                    return _authToken.GenerateJwtToken(userInfo.UserName);
+                }
+                else
+                {
+                    return "";
+                }
             }
+            return "";
 
         }
     }
