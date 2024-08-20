8409f472085456c71aae76a205026f10023702d5
chong wy <woonyap616@gmail.com>
2024-08-20 21:34:58 +0800

    redux refresh

--- project_base_react/src/App.js
+++ project_base_react/src/App.js
@@ -5,31 +5,40 @@ import MySidebar from './Control/MySidebar';
 import { MyPageContainer } from './Control/MyPageContainer';
 import Login from './View/login';
 import MyTopBar from './Control/MyTopBar';
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { initData, updateData, deleteData } from './Redux/actions';
-function App() {
+import { initData } from './Redux/actions';
 
+function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // localStorage.setItem('isLogin', isLogin);
-    dispatch(initData('isLogin', 'NotLogin'));
-  }, []);
-  var isLogin = useSelector((state) => state.isLogin);
-  console.log(isLogin);
+    // Load 'isLogin' from localStorage if it exists
+    const storedLoginStatus = localStorage.getItem('isLogin');
+    dispatch(initData('isLogin', storedLoginStatus || 'NotLogin'));
+  }, [dispatch]);
+
+  const isLogin = useSelector((state) => state.isLogin);
+
+  useEffect(() => {
+    // Persist 'isLogin' to localStorage whenever it changes
+    if (isLogin) {
+      localStorage.setItem('isLogin', isLogin);
+    }
+  }, [isLogin]);
+
   return (
     <div className='app-container'>
       {isLogin === 'Login' ? (
         <Router>
           <div className="app-container">
-            <MyTopBar/>
+            <MyTopBar />
             <div className="d-flex">
               <div className="my-sidebar">
-                <MySidebar/>
+                <MySidebar />
               </div>
               <div className="my-page-container">
-                <MyPageContainer/>
+                <MyPageContainer />
               </div>
             </div>
           </div>
